0b8577ff5ab6c789124d59e3792b695c9017f020
Johann Sebastian Schicho <sebastian-schicho@t-online.de>
2026-06-08 17:01:26 +0200

    neo4j ingestion script

---
+++ entities.py
@@ -0,0 +1,168 @@
+"""
+This models the basic entities of the MusicBrainz knowledge graph:
+Artist, Recording, Release, ReleaseGroup, and Genre.
+Relationships between these entities are not modeled here,
+but rather provided later through relationships in the graph database.
+
+The entities are defined as simplified versions of the data returned by the MusicBrainz API,
+containing only the most relevant fields for our use case.
+
+The basic relationships are as follows:
+- Artist -> Release (an artist has many releases)
+- Artist -> ReleaseGroup (an artist has many release groups)
+- Release -> Recording (a release has many recordings)
+- Anything -> Genre (artists, recordings, releases, and release groups can have many genres)
+
+The Genre hasGenre relationship additionally has a "count" property which indicates how strongly this genre is associated with the entity.
+This is derived from the "count" field in the MusicBrainz API, which indicates how many times this genre has been applied to the entity by users.
+"""
+
+import json
+
+
+class Genre:
+    def __init__(self, id: str, name: str, count: int):
+        self.id = id
+        self.name = name
+        self.count = count
+
+
+class Artist:
+    def __init__(
+        self, id: str, name: str, country: str, type: str, genres: list[Genre]
+    ):
+        self.id = id
+        self.name = name
+        self.country = country
+        self.type = type
+        self.genres = genres
+
+
+class Recording:
+    def __init__(self, id: str, title: str, length: int, genres: list[Genre]):
+        self.id = id
+        self.title = title
+        self.length = length
+        self.genres = genres
+
+
+class ReleaseGroup:
+    def __init__(
+        self,
+        id: str,
+        title: str,
+        first_release_date: str,
+        primary_type: str,
+        genres: list[Genre],
+    ):
+        self.id = id
+        self.title = title
+        self.first_release_date = first_release_date
+        self.primary_type = primary_type
+        self.genres = genres
+
+
+class Release:
+    def __init__(
+        self,
+        id: str,
+        title: str,
+        date: str,
+        country: str,
+        genres: list[Genre],
+        recordings: list[Recording],
+        release_group: ReleaseGroup,
+    ):
+        self.id = id
+        self.title = title
+        self.date = date
+        self.country = country
+        self.genres = genres
+        self.recordings = recordings
+        self.release_group = release_group
+
+
+def parse_releases_file(filepath: str) -> list[Release]:
+    with open(filepath, "r") as f:
+        data = json.load(f)
+    releases = []
+    for item in data:
+        release = Release(
+            id=item["id"],
+            title=item["title"],
+            date=item.get("date", ""),
+            country=item.get("country", ""),
+            genres=[
+                Genre(id=genre["id"], name=genre["name"], count=genre["count"])
+                for genre in item.get("genres", [])
+            ],
+            recordings=[
+                Recording(
+                    id=track["recording"]["id"],
+                    title=track["recording"]["title"],
+                    length=track["recording"]["length"],
+                    genres=[
+                        Genre(id=genre["id"], name=genre["name"], count=genre["count"])
+                        for genre in track["recording"].get("genres", [])
+                    ],
+                )
+                for track in item.get("media", [])[0].get("tracks", [])
+            ],
+            release_group=ReleaseGroup(
+                id=item["release-group"]["id"],
+                title=item["release-group"]["title"],
+                first_release_date=item["release-group"]["first-release-date"],
+                primary_type=item["release-group"]["primary-type"],
+                genres=[
+                    Genre(id=genre["id"], name=genre["name"], count=genre["count"])
+                    for genre in item["release-group"].get("genres", [])
+                ],
+            ),
+        )
+        releases.append(release)
+    return releases
+
+
+def parse_artists_file(filepath: str) -> list[Artist]:
+    with open(filepath, "r") as f:
+        data = json.load(f)
+    artists = []
+    for item in data:
+        artist = Artist(
+            id=item["id"],
+            name=item["name"],
+            country=item.get("country", ""),
+            type=item.get("type", ""),
+            genres=[
+                # no id for tags, from the search endpoint, so we set it to None
+                Genre(id=None, name=tag["name"], count=tag["count"])
+                for tag in item.get("tags", [])
+            ],
+        )
+        artists.append(artist)
+    return artists
+
+
+def parse_genres_file(filepath: str) -> list[Genre]:
+    with open(filepath, "r") as f:
+        data = json.load(f)
+    genres = []
+    for item in data:
+        # this is to setup the genre entities for the graph. No relationships, so we set count to None.
+        genre = Genre(id=item["id"], name=item["name"], count=None)
+        genres.append(genre)
+    return genres
+
+
+if __name__ == "__main__":
+    test_file = (
+        "dataset/releases/r_scott_garcia_01cfa586-2a9a-46c8-95ab-7b05e25eae62.json"
+    )
+
+    releases = parse_releases_file(test_file)
+    for release in releases:
+        print(json.dumps(release.__dict__, default=lambda o: o.__dict__, indent=2))
+
+    artists = parse_artists_file("dataset/artists.json")
+    for artist in artists:
+        print(json.dumps(artist.__dict__, default=lambda o: o.__dict__, indent=2))
--- graph.py
+++ graph.py
@@ -1,5 +1,17 @@
 from neo4j import Driver, GraphDatabase
-from config import NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD
+import os
+
+from config import NEO4J_PASSWORD, NEO4J_URI, NEO4J_USER
+from entities import (
+    Artist,
+    Genre,
+    Recording,
+    Release,
+    ReleaseGroup,
+    parse_artists_file,
+    parse_genres_file,
+    parse_releases_file,
+)
 
 
 def get_driver() -> Driver:
@@ -11,5 +23,179 @@ def clear_graph(driver: Driver):
         s.run("MATCH (n) DETACH DELETE n")
 
 
+def create_constraints(driver):
+    with driver.session() as s:
+        s.run("CREATE CONSTRAINT IF NOT EXISTS FOR (g:Genre) REQUIRE g.id IS UNIQUE")
+        s.run("CREATE CONSTRAINT IF NOT EXISTS FOR (g:Genre) REQUIRE g.name IS UNIQUE")
+        s.run("CREATE CONSTRAINT IF NOT EXISTS FOR (a:Artist) REQUIRE a.id IS UNIQUE")
+        s.run(
+            "CREATE CONSTRAINT IF NOT EXISTS FOR (rec:Recording) REQUIRE rec.id IS UNIQUE"
+        )
+        s.run(
+            "CREATE CONSTRAINT IF NOT EXISTS FOR (rel:Release) REQUIRE rel.id IS UNIQUE"
+        )
+        s.run(
+            "CREATE CONSTRAINT IF NOT EXISTS FOR (rg:ReleaseGroup) REQUIRE rg.id IS UNIQUE"
+        )
+
+
+def create_genre_nodes(driver: Driver, genres: list[Genre]):
+    with driver.session() as s:
+        for genre in genres:
+            print(f"Creating genre node for {genre.name} ({genre.id})")
+            s.run(
+                "MERGE (g:Genre {id: $id}) SET g.name = $name",
+                id=genre.id,
+                name=genre.name,
+            )
+
+
+def load_genres(driver: Driver, filepath: str):
+    genres = parse_genres_file(filepath)
+    create_genre_nodes(driver, genres)
+
+
+def create_artist_nodes(driver: Driver, artists: list[Artist]):
+    with driver.session() as s:
+        for artist in artists:
+            print(f"Creating artist node for {artist.name} ({artist.id})")
+            # create artist node
+            s.run(
+                "MERGE (a:Artist {id: $id}) SET a.name = $name, a.type = $type, a.country = $country",
+                id=artist.id,
+                name=artist.name,
+                type=artist.type,
+                country=artist.country,
+            )
+
+            # create relationships to genres (we have to find them by name, since we don't have the id in the artist data)
+            for genre in artist.genres:
+                s.run(
+                    "MATCH (a:Artist {id: $artist_id}), (g:Genre {name: $genre_name}) "
+                    "MERGE (a)-[:HAS_GENRE]->(g)",
+                    artist_id=artist.id,
+                    genre_name=genre.name,
+                )
+
+
+def load_artists(driver: Driver, filepath: str):
+    artists = parse_artists_file(filepath)
+    create_artist_nodes(driver, artists)
+
+
+def create_release_release_group_recording_nodes(
+    driver: Driver, releases: list[Release], artist_id: str
+):
+    with driver.session() as s:
+        for release in releases:
+            # create release node
+            s.run(
+                "MERGE (r:Release {id: $id}) SET r.title = $title, r.date = $date, r.country = $country",
+                id=release.id,
+                title=release.title,
+                date=release.date,
+                country=release.country,
+            )
+
+            # create relationships to genres
+            for genre in release.genres:
+                s.run(
+                    "MATCH (r:Release {id: $release_id}), (g:Genre {id: $genre_id}) "
+                    "MERGE (r)-[:HAS_GENRE {count: $count}]->(g)",
+                    release_id=release.id,
+                    genre_id=genre.id,
+                    count=genre.count,
+                )
+
+            # create relationship from artist to release
+            s.run(
+                "MATCH (a:Artist {id: $artist_id}), (r:Release {id: $release_id}) "
+                "MERGE (a)-[:ARTIST_OF]->(r)",
+                artist_id=artist_id,
+                release_id=release.id,
+            )
+
+            # create release group node
+            s.run(
+                """MERGE (rg:ReleaseGroup {id: $id})
+                SET rg.title = $title, rg.first_release_date = $first_release_date, rg.primary_type = $primary_type""",
+                id=release.release_group.id,
+                title=release.release_group.title,
+                first_release_date=release.release_group.first_release_date,
+                primary_type=release.release_group.primary_type,
+            )
+
+            # create relationships to genres
+            for genre in release.release_group.genres:
+                s.run(
+                    "MATCH (rg:ReleaseGroup {id: $release_group_id}), (g:Genre {id: $genre_id}) "
+                    "MERGE (rg)-[:HAS_GENRE {count: $count}]->(g)",
+                    release_group_id=release.release_group.id,
+                    genre_id=genre.id,
+                    count=genre.count,
+                )
+
+            s.run(
+                "MATCH (a:Artist {id: $artist_id}), (rg:ReleaseGroup {id: $release_group_id}) "
+                "MERGE (a)-[:ARTIST_OF]->(rg)",
+                artist_id=artist_id,
+                release_group_id=release.release_group.id,
+            )
+
+            # create relationship from release group to release
+            s.run(
+                "MATCH (rg:ReleaseGroup {id: $release_group_id}), (r:Release {id: $release_id}) "
+                "MERGE (rg)-[:HAS_RELEASE]->(r)",
+                release_group_id=release.release_group.id,
+                release_id=release.id,
+            )
+
+            # create recording nodes and relationships to release and genres
+            for recording in release.recordings:
+                s.run(
+                    "MERGE (rec:Recording {id: $id}) SET rec.title = $title, rec.length = $length",
+                    id=recording.id,
+                    title=recording.title,
+                    length=recording.length,
+                )
+                s.run(
+                    "MATCH (r:Release {id: $release_id}), (rec:Recording {id: $recording_id}) "
+                    "MERGE (r)-[:HAS_RECORDING]->(rec)",
+                    recording_id=recording.id,
+                    release_id=release.id,
+                )
+                for genre in recording.genres:
+                    s.run(
+                        "MATCH (rec:Recording {id: $recording_id}), (g:Genre {id: $genre_id}) "
+                        "MERGE (rec)-[:HAS_GENRE {count: $count}]->(g)",
+                        recording_id=recording.id,
+                        genre_id=genre.id,
+                        count=genre.count,
+                    )
+
+                s.run(
+                    "MATCH (a:Artist {id: $artist_id}), (rec:Recording {id: $recording_id}) "
+                    "MERGE (a)-[:ARTIST_OF]->(rec)",
+                    artist_id=artist_id,
+                    recording_id=recording.id,
+                )
+
+
+def load_releases(driver: Driver, filepath: str):
+    # find all files in the releases directory
+    files = [f for f in os.listdir(filepath) if f.endswith(".json")]
+    for file in files:
+        artist_id = file.split("_")[-1].split(".")[0]
+        print(f"Loading releases for artist {artist_id} from file {file}")
+        releases = parse_releases_file(os.path.join(filepath, file))
+        create_release_release_group_recording_nodes(driver, releases, artist_id)
+
+
 if __name__ == "__main__":
-    pass
+    driver = get_driver()
+    create_constraints(driver)
+    clear_graph(driver)
+    load_genres(driver, "dataset/genres.json")
+    load_artists(driver, "dataset/artists.json")
+    load_releases(driver, "dataset/releases")
+    driver.close()
