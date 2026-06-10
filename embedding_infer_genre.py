fd19176a504ccdc855ae84d181a3ef5450da5d20
Johann Sebastian Schicho <sebastian-schicho@t-online.de>
2026-06-10 23:06:40 +0200

    adjust embedding process and evaluation to focus only on performance of tail prediction of HAS_GENRE

--- embedding_infer_genre.py
+++ embedding_infer_genre.py
@@ -21,16 +21,27 @@ def train_model(
     model_name: str = "DistMult",
     training: TriplesFactory = None,
     testing: TriplesFactory = None,
+    validation: TriplesFactory = None,
     epochs: int = DEFAULT_EPOCHS,
 ) -> PipelineResult:
 
     result = pipeline(
         training=training,
         testing=testing,
+        validation=validation,
         model=model_name,
         optimizer="Adam",
         random_seed=DEFAULT_RANDOM_SEED,
         epochs=epochs,
+        # Stop early if top 10 hits does not improve.
+        # In fact the model does overfit on all relations when using TransE,
+        # diminishing the performance on the HAS_GENRE relation.
+        stopper="early",
+        stopper_kwargs=dict(
+            frequency=5,
+            patience=3,
+            relative_delta=0.002,
+        ),
     )
 
     return result
@@ -40,9 +51,15 @@ def evaluate_model(
     result: PipelineResult,
     relation: str = "HAS_GENRE",
     testing: TriplesFactory = None,
+    validation_filter_out: TriplesFactory = None,
 ):
     """
     https://pykeen.readthedocs.io/en/stable/tutorial/understanding_evaluation.html
+
+    Especially in regards to the additional_filter_triples argument,
+    which allows us to filter out all triples that were seen during training and validation,
+    ensuring a more realistic evaluation of the model's performance on unseen data.
+    https://pykeen.readthedocs.io/en/stable/tutorial/understanding_evaluation.html#custom-training-loops
     """
 
     # Restrict the  set to only triples with the HAS_GENRE relation
@@ -54,18 +71,30 @@ def evaluate_model(
         )
 
     evaluator = RankBasedEvaluator(
-        metrics=[HitsAtK(k=10), InverseHarmonicMeanRank()],
+        metrics=[
+            HitsAtK(k=1),
+            HitsAtK(k=3),
+            HitsAtK(k=5),
+            HitsAtK(k=10),
+            InverseHarmonicMeanRank(),
+        ],
     )
 
     metrics = evaluator.evaluate(
         model=result.model,
         mapped_triples=testing.mapped_triples,
-        additional_filter_triples=result.training.mapped_triples,
+        additional_filter_triples=[
+            result.training.mapped_triples,
+            validation_filter_out.mapped_triples,
+        ],
     ).to_flat_dict()
 
     print("\n" + "=" * 80)
     print("Evaluation results (tail prediction - genre prediction):")
     print(f"Evaluation on {relation} ({testing.num_triples} test triples)")
+    print(f"Hits@1: {metrics['tail.realistic.hits_at_1']:.4f}")
+    print(f"Hits@3: {metrics['tail.realistic.hits_at_3']:.4f}")
+    print(f"Hits@5: {metrics['tail.realistic.hits_at_5']:.4f}")
     print(f"Hits@10: {metrics['tail.realistic.hits_at_10']:.4f}")
     print(f"MRR: {metrics['tail.realistic.inverse_harmonic_mean_rank']:.4f}")
 
@@ -97,11 +126,31 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     tf = load_triples(TRIPLES_PATH)
-    training, testing = tf.split(random_state=DEFAULT_RANDOM_SEED)
+    training, testing, validation = tf.split(
+        [0.8, 0.1, 0.1], random_state=DEFAULT_RANDOM_SEED
+    )
+
+    print(
+        f"Split sizes: training={training.num_triples}, testing={testing.num_triples}, validation={validation.num_triples}"
+    )
+
+    # restrict the validation set to only triples with the HAS_GENRE relation,
+    # as we are only interested in evaluating the model on this relation.
+    testing = testing.new_with_restriction(relations=["HAS_GENRE"])
+    validation = validation.new_with_restriction(relations=["HAS_GENRE"])
+
+    print(
+        f"Split sizes after restriction: training={training.num_triples}, testing={testing.num_triples}, validation={validation.num_triples}"
+    )
 
     pk_result = train_model(
-        model_name=args.model, training=training, testing=testing, epochs=args.epochs
+        model_name=args.model,
+        training=training,
+        testing=testing,
+        validation=validation,
+        epochs=args.epochs,
     )
-    evaluate_model(pk_result, testing=testing)
+
+    evaluate_model(pk_result, testing=testing, validation_filter_out=validation)
 
     save_model(pk_result, args.model, SAVE_BASE_PATH)
